a8f347fd69a1ee858248464b2095b48eae776e77
Aleksandrs Pučenkins <90133296+byGromov@users.noreply.github.com>
2023-04-02 21:03:46 +0000

    fix1

--- main.py
+++ main.py
@@ -1,39 +1,24 @@
 # python3
 # Author: Aleksandrs Pučenkins 17.gr. 221RDB335
 
-ddef parallel_processing(n, m, data):
+def parallel_processing(n, m, data):
+
     output = []
-
-    thread_execution_times = []
-    for i in range(n):
-        thread_execution_times.append(0)
-
-    output.append((0, 0))
-    thread_execution_times[0] = data[0]
-
-    for i in range(m)[1:]:
-        n_min = 0
-        for k in range(n)[1:]:
-            if thread_execution_times[k] < thread_execution_times[n_min]:
-                n_min = k
-        output.append((n_min, thread_execution_times[n_min]))
-        thread_execution_times[n_min] = thread_execution_times[n_min] + data[i]
+    threads = [(i, 0) for i in range(n)] 
+    
+    for i in range(m):
+        t = data[i]
+        thread = min(threads, key=lambda x: x[1])  
+        output.append((thread[0], thread[1]))  
+        threads[threads.index(thread)] = (thread[0], thread[1] + t)  
     return output
 
 def main():
-    n = 0
-    m = 0
-    (n, m) = tuple(map(int, input().split()))
-
-    data = []
+    n, m = map(int, input().split())
     data = list(map(int, input().split()))
-
-    result = parallel_processing(n,m,data)
-    
-    for pair in result:
-        print(str(pair[0]) + " " + str(pair[1]))
-
-
-
+    result = parallel_processing(n, m, data)
+    for t in result:
+        print(t[0], t[1])
+        
 if __name__ == "__main__":
     main()
